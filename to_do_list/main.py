771a482c96f5704a7f53fc4ce0fd3c0b0184cdf9
Gthtjtkt <nonburritobro@gmail.com>
2026-01-09 20:01:31 -0600

    Updating main with delete and load options

--- to_do_list/main.py
+++ to_do_list/main.py
@@ -4,7 +4,7 @@ import os
 import file_manager as fm
 import ui_utils as ui
 import tkinter as tk
-from tkinter import messagebox, simpledialog
+from tkinter import messagebox, simpledialog, filedialog
 
 class TodoApp:
     def __init__(self, root):
@@ -22,13 +22,17 @@ class TodoApp:
         self.label_status.pack(pady = 10)
 
         # -------- Listbox to show tasks ---------
+        # Is rendered above the options menu
+        # Displays the tasks in the current list
         self.task_listbox = tk.Listbox(root, width=50, height=20)
         self.task_listbox.pack(pady = 10, padx = 15)
 
         # -------- Buttons --------------
         tk.Button(root, text="New List", command=self.create_new_list).pack(fill = "x", padx = 50)
+        tk.Button(root, text="Load List", command=self.load_list).pack(fill = "x", padx = 50)
         tk.Button(root, text="Add Task", command=self.add_task).pack(fill = "x", padx = 50)
-        tk.Button(root, text="Save Current List", command=self.save_list).pack(fill = "x", padx = 50)
+        tk.Button(root, text="Delete Task", command=self.delete_task).pack(fill = "x", padx = 50)
+        tk.Button(root, text="Save Active List", command=self.save_list).pack(fill = "x", padx = 50)
 
         # ------- Menu Bar ----------
         self.setup_menu()
@@ -37,9 +41,12 @@ class TodoApp:
         menubar = tk.Menu(self.root)
 
         # File Menu
+        # Appears in the top right corner of the window
         file_menu = tk.Menu(menubar, tearoff = 0)
         file_menu.add_command(label = "New List", command=self.create_new_list)
         file_menu.add_command(label='Save', command=self.save_list)
+        file_menu.add_command(label='Load', command=self.load_list)
+        file_menu.add_command(label='Delete Task', command=self.delete_task)
         file_menu.add_separator()
         file_menu.add_command(label='Exit', command=self.root.quit)
         menubar.add_cascade(label='File', menu=file_menu)
@@ -66,6 +73,34 @@ class TodoApp:
             self.current_tasks.append(task)
             self.task_listbox.insert(tk.END, task)
 
+    def delete_task(self):
+        # make sure list isn't active
+        if not self.current_filename:
+            messagebox.showerror("Error", "No Active List.")
+            return
+
+        # make sure task is selected
+        selection = self.task_listbox.curselection() # user selects what task to delete with their cursor
+        if not selection:
+            messagebox.showwarning("No Selection", "Select a task to delete.")
+            return
+
+        index = selection[0]
+        task = self.current_tasks[index]
+
+        # Confirm the user actually wants to delete the task
+        confirm = messagebox.askyesno(
+            "Confirm delete",
+            f"Delete this task? \n\n{task}"
+        )
+
+        if confirm:
+            # remove from list
+            self.current_tasks.pop(index)
+
+            # Remove from UI
+            self.task_listbox.delete(index)
+
     def save_list(self):
         if self.current_filename:
             fm.write_lines(self.current_filename, self.current_tasks)
@@ -73,11 +108,44 @@ class TodoApp:
         else:
             messagebox.showerror("Error", "No active list to save.")
 
+    def load_list(self):
+
+        filepath = filedialog.askopenfilename(
+            title="Load To-Do List:",
+            initialdir=self.app_dir,
+            filetypes=[("Test Files", "*.txt")]
+        )
+
+        if not filepath:
+            return
+
+        try:
+            tasks = fm.read_lines(filepath)
+
+            self.current_filename = filepath
+            self.current_tasks = tasks
+
+            self.task_listbox.delete(0, tk.END)
+            for task in tasks:
+                self.task_listbox.insert(tk.END, task)
+
+            list_name = os.path.basename(filepath)
+            self.label_status.config(text=f"Active List: {list_name}")
+
+            messagebox.showinfo("Loaded", f"Loaded List: {list_name}")
+
+        except Exception as e:
+            messagebox.showerror("Error", f"Error while loading list: \n{e}")
+
     def update_ui(self, name):
         self.label_status.config(text=f"Active list: {name}")
         self.task_listbox.delete(0, tk.END)
 
 if __name__ == "__main__":
+
+    # Create instance of the tkinter ui object and name it root
     root = tk.Tk()
+
+    # pass this
     app = TodoApp(root)
     root.mainloop()
