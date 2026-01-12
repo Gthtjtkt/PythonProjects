9479944f686740630f9e0164e5f8eeb90d4bb4c9
Gthtjtkt <nonburritobro@gmail.com>
2026-01-11 22:17:59 -0600

    adding readme and comments to ui_utils ... sidenote, it isn't used for anything lol

--- to_do_list/ui_utils.py
+++ to_do_list/ui_utils.py
@@ -1,8 +1,19 @@
 # ui_utils.py
+"""
+This module contains 'Utility' functions for the User Interface.
+These are generic tools that help the main application interact
+with the user via pop-up windows or the console.
+"""
 import tkinter as tk
 from tkinter import filedialog
 
 def select_directory_via_gui():
+    """
+    Opens a standard Windows/Mac/Linux folder selection dialog.
+    This allows the user to pick a folder with their mouse instead
+    of typing a path like 'C:/Users/Documents'.
+    """
+    # 1. We temporarily start the Tkinter engine
     root = tk.Tk()
     root.withdraw()
     path = filedialog.askdirectory()
